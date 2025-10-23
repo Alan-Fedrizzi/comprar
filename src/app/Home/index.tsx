1343d54f5621384ce78682ac26d46d5b4e551059
alanfedrizzi <alanfedrizzi@gmail.com>
2025-10-23 11:46:05 +0200

    item storage

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,9 +1,10 @@
+import { useState, useEffect } from "react";
 import {
+  Alert,
   View,
   Image,
   TouchableOpacity,
   Text,
-  ScrollView,
   FlatList,
 } from "react-native";
 import { styles } from "./styles";
@@ -13,33 +14,84 @@ import { Input } from "@/components/Input";
 import { Filter } from "@/components/Filter";
 import { FilterStatus } from "@/types/FilterStatus";
 import { Item } from "@/components/Item";
+import { itemsStorage, ItemStorage } from "@/storage/itemsStorage";
 
 const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE];
 
-const ITEMS = [
-  {
-    id: "1",
-    status: FilterStatus.DONE,
-    description: "Comprar café... quiero café...",
-  },
-  {
-    id: "2",
-    status: FilterStatus.PENDING,
-    description: "fazer coisas..",
-  },
-  {
-    id: "3",
-    status: FilterStatus.DONE,
-    description: "Comprar e tal..",
-  },
-  {
-    id: "4",
-    status: FilterStatus.PENDING,
-    description: "lavanderia",
-  },
-];
+// const ITEMS = [
+//   {
+//     id: "1",
+//     status: FilterStatus.DONE,
+//     description: "Comprar café... quiero café...",
+//   },
+//   {
+//     id: "2",
+//     status: FilterStatus.PENDING,
+//     description: "fazer coisas..",
+//   },
+//   {
+//     id: "3",
+//     status: FilterStatus.DONE,
+//     description: "Comprar e tal..",
+//   },
+//   {
+//     id: "4",
+//     status: FilterStatus.PENDING,
+//     description: "lavanderia",
+//   },
+// ];
 
 export default function Home() {
+  const [filter, setFilter] = useState(FilterStatus.PENDING);
+  const [description, setDescription] = useState("");
+  const [items, setItems] = useState<ItemStorage[]>([]);
+
+  async function handleAddItems() {
+    if (!description.trim()) {
+      return Alert.alert("Adicionar", "Informe a descrição para adicionar.");
+    }
+
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING,
+    };
+
+    // setItems((prevState) => [...prevState, newItem]);
+    await itemsStorage.add(newItem);
+    // abaixo, voltamos o filtro para pending, o que vai disparar o useEffect, e varregar os itens pendentes, isso funciona só qd estamos em comprados e adicionamos um item
+    // para garantir, deixamos getItemsByStatus
+    await getItemsByStatus();
+    setFilter(FilterStatus.PENDING);
+    Alert.alert("Adicionar", `Adicionado ${description}`);
+    setDescription("");
+  }
+
+  async function getItemsByStatus() {
+    try {
+      const response = await itemsStorage.getByStatus(filter);
+      setItems(response);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Erro", "Não foi possível filtrar os itens.");
+    }
+  }
+
+  async function handleRemoveItem(id: string) {
+    try {
+      await itemsStorage.remove(id);
+      await getItemsByStatus();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Remover", "Não foi possível removero o item.");
+    }
+  }
+
+  useEffect(() => {
+    // itemsStorage.get().then(response => console.log(response));
+    getItemsByStatus();
+  }, [filter]);
+
   return (
     <View style={styles.container}>
       {/* não precisamos especificar qual imagem vamos usar (2x ou 3x), o react native decide baseado no dispositivo
@@ -47,8 +99,14 @@ export default function Home() {
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
       <View style={styles.form}>
-        <Input placeholder="O que você precisa comprar?" />
-        <Button title="Entrar" />
+        <Input
+          placeholder="O que você precisa comprar?"
+          value={description} // para funcionar o clear
+          onChangeText={setDescription}
+          // onChangeText={(value) => setDescription(value)}
+        />
+
+        <Button title="Acicionar" onPress={handleAddItems} />
       </View>
 
       <View style={styles.content}>
@@ -57,7 +115,12 @@ export default function Home() {
         <Filter status={FilterStatus.DONE} isActive={false} /> */}
         <View style={styles.header}>
           {FILTER_STATUS.map((status) => (
-            <Filter status={status} isActive key={status} />
+            <Filter
+              status={status}
+              isActive={status === filter}
+              key={status}
+              onPress={() => setFilter(status)}
+            />
           ))}
 
           <TouchableOpacity style={styles.clearButton} activeOpacity={0.8}>
@@ -68,13 +131,14 @@ export default function Home() {
         {/* FlatList - renderiza somente os itens que aparecem na tela, melhor para performance (já tem scroll) */}
         <FlatList
           // data={[]}
-          data={ITEMS}
+          // data={ITEMS}
+          data={items}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
             <Item
               data={item}
               onChangeStatus={() => console.log("alterar status..")}
-              onRemove={() => console.log("remover..")}
+              onRemove={() => handleRemoveItem(item.id)}
             />
           )}
           showsVerticalScrollIndicator={false}
